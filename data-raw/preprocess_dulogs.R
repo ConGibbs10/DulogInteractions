5cd33f36ff28f6540d635a6c32acd0f3c16b4a3e
congibbs10 <congibbs10@gmail.com>
2022-08-20 00:25:25 -0600

    Remove the time epsilon of 3.99.

--- data-raw/preprocess_dulogs.R
+++ data-raw/preprocess_dulogs.R
@@ -75,22 +75,3 @@ preprocess_dulogs(
   log = 'logs/preprocess_dulogs/time_eps3/default'
 )
 tictoc::toc()
-
-# with time epsilon of 3.99:
-tictoc::tic()
-## read in the time epsilon of 3.99
-read_step <- readr::read_rds('logs/read_dulogs/time_eps3p99.rds')
-dulogs <- read_step$return
-## create directory within preprocess_dulogs
-dir.create('logs/preprocess_dulogs/time_eps3p99/')
-## run preprocessing
-preprocess_dulogs(
-  dulogs,
-  min_rssi = -Inf,
-  max_rssi = 15,
-  only_barn = FALSE,
-  start_time = '0000-01-01 UTC',
-  end_time = '9999-01-01 UTC',
-  log = 'logs/preprocess_dulogs/time_eps3p99/default'
-)
-tictoc::toc()
