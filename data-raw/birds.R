28cc416c9d4a16bcaeaf2d66df40f014dd667ab9
congibbs10 <congibbs10@gmail.com>
2022-08-31 16:57:34 -0600

    Update raw package data, manual, and vignette.

--- data-raw/birds.R
+++ data-raw/birds.R
@@ -1,5 +1,26 @@
 # read birds from csv
 birds <- readr::read_csv('data-raw/birds-raw.csv',
-                         col_types = 'ccicccc')
+                         col_types = 'cccccccccncccccc?ncncncncnc')
+
+# edits:
+## create logical columns out of logical features
+## turn date captured into a date object
+birds <- dplyr::mutate(
+  birds,
+  dplyr::across(
+    c(
+      'main_barn',
+      'recap',
+      'feathers_sampled',
+      'blood_sampled',
+      'malaria',
+      'lts_intact',
+      'rts_intact'
+    ),
+    ~ dplyr::case_when(.x == 'Y' ~ TRUE, .x == 'N' ~ FALSE, TRUE ~ NA)
+  ),
+  date_captured = lubridate::mdy(date_captured)
+)
+
 # write data
 usethis::use_data(birds, overwrite = TRUE)
