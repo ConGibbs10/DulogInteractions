96ae34ef47d307fb1f366c81f61bcbeb6692c548
congibbs10 <congibbs10@gmail.com>
2022-09-15 13:06:44 -0600

    Generate common logs with raw data.

--- data-raw/preprocess_dulogs.R
+++ data-raw/preprocess_dulogs.R
@@ -1,77 +1,261 @@
 library(DulogInteractions)
 
-# with time epsilon of 0:
-tictoc::tic()
-## read in the time epsilon of 0
-read_step <- readr::read_rds('logs/read_dulogs/time_eps0.rds')
+################################################################################
+# timeeps2_barnruleor
+################################################################################
+
+## read in the time epsilon of 2 and barn rule of or
+read_step <- readr::read_rds('logs/read_dulogs/timeeps2_barnruleor.rds')
 dulogs <- read_step$return
 ## create directory within preprocess_dulogs
-dir.create('logs/preprocess_dulogs/time_eps0/')
-## run preprocessing
+dir.create('logs/preprocess_dulogs/timeeps2_barnruleor/', recursive = TRUE)
+
+#-------------------------------------------------------------------------------
+# only barn is FALSE
+#-------------------------------------------------------------------------------
+
+tictoc::tic()
+## run preprocessing with time epsilon of 0 and only_barn = FALSE
 preprocess_dulogs(
   dulogs,
   min_rssi = -Inf,
   max_rssi = 15,
-  only_barn = FALSE,
   start_time = '0000-01-01 UTC',
   end_time = '9999-01-01 UTC',
-  log = 'logs/preprocess_dulogs/time_eps0/default'
+  only_barn = FALSE,
+  time_eps = 0,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleor/timeeps0_onlybarnF'
 )
 tictoc::toc()
 
-# with time epsilon of 1:
 tictoc::tic()
-## read in the time epsilon of 1
-read_step <- readr::read_rds('logs/read_dulogs/time_eps1.rds')
-dulogs <- read_step$return
-## create directory within preprocess_dulogs
-dir.create('logs/preprocess_dulogs/time_eps1/')
-## run preprocessing
+## run preprocessing with time epsilon of 1 and only_barn = FALSE
 preprocess_dulogs(
   dulogs,
   min_rssi = -Inf,
   max_rssi = 15,
-  only_barn = FALSE,
   start_time = '0000-01-01 UTC',
   end_time = '9999-01-01 UTC',
-  log = 'logs/preprocess_dulogs/time_eps1/default'
+  only_barn = FALSE,
+  time_eps = 1,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleor/timeeps1_onlybarnF'
 )
 tictoc::toc()
 
-# with time epsilon of 2:
 tictoc::tic()
-## read in the time epsilon of 2
-read_step <- readr::read_rds('logs/read_dulogs/time_eps2.rds')
-dulogs <- read_step$return
-## create directory within preprocess_dulogs
-dir.create('logs/preprocess_dulogs/time_eps2/')
-## run preprocessing
+## run preprocessing with time epsilon of 2 and only_barn = FALSE
 preprocess_dulogs(
   dulogs,
   min_rssi = -Inf,
   max_rssi = 15,
-  only_barn = FALSE,
   start_time = '0000-01-01 UTC',
   end_time = '9999-01-01 UTC',
-  log = 'logs/preprocess_dulogs/time_eps2/default'
+  only_barn = FALSE,
+  time_eps = 2,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleor/timeeps2_onlybarnF'
 )
 tictoc::toc()
 
-# with time epsilon of 3:
 tictoc::tic()
-## read in the time epsilon of 3
-read_step <- readr::read_rds('logs/read_dulogs/time_eps3.rds')
-dulogs <- read_step$return
-## create directory within preprocess_dulogs
-dir.create('logs/preprocess_dulogs/time_eps3/')
-## run preprocessing
+## run preprocessing with time epsilon of 3 and only_barn = FALSE
 preprocess_dulogs(
   dulogs,
   min_rssi = -Inf,
   max_rssi = 15,
-  only_barn = FALSE,
   start_time = '0000-01-01 UTC',
   end_time = '9999-01-01 UTC',
-  log = 'logs/preprocess_dulogs/time_eps3/default'
+  only_barn = FALSE,
+  time_eps = 3,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleor/timeeps3_onlybarnF'
+)
+tictoc::toc()
+
+#-------------------------------------------------------------------------------
+# only barn is TRUE
+#-------------------------------------------------------------------------------
+
+tictoc::tic()
+## run preprocessing with time epsilon of 0 and only_barn = TRUE
+preprocess_dulogs(
+  dulogs,
+  min_rssi = -Inf,
+  max_rssi = 15,
+  start_time = '0000-01-01 UTC',
+  end_time = '9999-01-01 UTC',
+  only_barn = TRUE,
+  time_eps = 0,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleor/timeeps0_onlybarnT'
+)
+tictoc::toc()
+
+tictoc::tic()
+## run preprocessing with time epsilon of 1 and only_barn = TRUE
+preprocess_dulogs(
+  dulogs,
+  min_rssi = -Inf,
+  max_rssi = 15,
+  start_time = '0000-01-01 UTC',
+  end_time = '9999-01-01 UTC',
+  only_barn = TRUE,
+  time_eps = 1,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleor/timeeps1_onlybarnT'
+)
+tictoc::toc()
+
+tictoc::tic()
+## run preprocessing with time epsilon of 2 and only_barn = TRUE
+preprocess_dulogs(
+  dulogs,
+  min_rssi = -Inf,
+  max_rssi = 15,
+  start_time = '0000-01-01 UTC',
+  end_time = '9999-01-01 UTC',
+  only_barn = TRUE,
+  time_eps = 2,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleor/timeeps2_onlybarnT'
+)
+tictoc::toc()
+
+tictoc::tic()
+## run preprocessing with time epsilon of 3 and only_barn = TRUE
+preprocess_dulogs(
+  dulogs,
+  min_rssi = -Inf,
+  max_rssi = 15,
+  start_time = '0000-01-01 UTC',
+  end_time = '9999-01-01 UTC',
+  only_barn = TRUE,
+  time_eps = 3,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleor/timeeps3_onlybarnT'
+)
+tictoc::toc()
+
+################################################################################
+# timeeps2_barnruleand
+################################################################################
+
+## read in the time epsilon of 2 and barn rule of or
+read_step <- readr::read_rds('logs/read_dulogs/timeeps2_barnruleand.rds')
+dulogs <- read_step$return
+## create directory within preprocess_dulogs
+dir.create('logs/preprocess_dulogs/timeeps2_barnruleand/', recursive = TRUE)
+
+#-------------------------------------------------------------------------------
+# only barn is FALSE
+#-------------------------------------------------------------------------------
+
+tictoc::tic()
+## run preprocessing with time epsilon of 0 and only_barn = FALSE
+preprocess_dulogs(
+  dulogs,
+  min_rssi = -Inf,
+  max_rssi = 15,
+  start_time = '0000-01-01 UTC',
+  end_time = '9999-01-01 UTC',
+  only_barn = FALSE,
+  time_eps = 0,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleand/timeeps0_onlybarnF'
+)
+tictoc::toc()
+
+tictoc::tic()
+## run preprocessing with time epsilon of 1 and only_barn = FALSE
+preprocess_dulogs(
+  dulogs,
+  min_rssi = -Inf,
+  max_rssi = 15,
+  start_time = '0000-01-01 UTC',
+  end_time = '9999-01-01 UTC',
+  only_barn = FALSE,
+  time_eps = 1,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleand/timeeps1_onlybarnF'
+)
+tictoc::toc()
+
+tictoc::tic()
+## run preprocessing with time epsilon of 2 and only_barn = FALSE
+preprocess_dulogs(
+  dulogs,
+  min_rssi = -Inf,
+  max_rssi = 15,
+  start_time = '0000-01-01 UTC',
+  end_time = '9999-01-01 UTC',
+  only_barn = FALSE,
+  time_eps = 2,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleand/timeeps2_onlybarnF'
+)
+tictoc::toc()
+
+tictoc::tic()
+## run preprocessing with time epsilon of 3 and only_barn = FALSE
+preprocess_dulogs(
+  dulogs,
+  min_rssi = -Inf,
+  max_rssi = 15,
+  start_time = '0000-01-01 UTC',
+  end_time = '9999-01-01 UTC',
+  only_barn = FALSE,
+  time_eps = 3,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleand/timeeps3_onlybarnF'
+)
+tictoc::toc()
+
+#-------------------------------------------------------------------------------
+# only barn is TRUE
+#-------------------------------------------------------------------------------
+
+tictoc::tic()
+## run preprocessing with time epsilon of 0 and only_barn = TRUE
+preprocess_dulogs(
+  dulogs,
+  min_rssi = -Inf,
+  max_rssi = 15,
+  start_time = '0000-01-01 UTC',
+  end_time = '9999-01-01 UTC',
+  only_barn = TRUE,
+  time_eps = 0,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleand/timeeps0_onlybarnT'
+)
+tictoc::toc()
+
+tictoc::tic()
+## run preprocessing with time epsilon of 1 and only_barn = TRUE
+preprocess_dulogs(
+  dulogs,
+  min_rssi = -Inf,
+  max_rssi = 15,
+  start_time = '0000-01-01 UTC',
+  end_time = '9999-01-01 UTC',
+  only_barn = TRUE,
+  time_eps = 1,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleand/timeeps1_onlybarnT'
+)
+tictoc::toc()
+
+tictoc::tic()
+## run preprocessing with time epsilon of 2 and only_barn = TRUE
+preprocess_dulogs(
+  dulogs,
+  min_rssi = -Inf,
+  max_rssi = 15,
+  start_time = '0000-01-01 UTC',
+  end_time = '9999-01-01 UTC',
+  only_barn = TRUE,
+  time_eps = 2,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleand/timeeps2_onlybarnT'
+)
+tictoc::toc()
+
+tictoc::tic()
+## run preprocessing with time epsilon of 3 and only_barn = TRUE
+preprocess_dulogs(
+  dulogs,
+  min_rssi = -Inf,
+  max_rssi = 15,
+  start_time = '0000-01-01 UTC',
+  end_time = '9999-01-01 UTC',
+  only_barn = TRUE,
+  time_eps = 3,
+  log = 'logs/preprocess_dulogs/timeeps2_barnruleand/timeeps3_onlybarnT'
 )
 tictoc::toc()
--- data-raw/read_dulogs.R
+++ data-raw/read_dulogs.R
@@ -3,7 +3,11 @@ library(DulogInteractions)
 # paths and patterns
 paths <- c(mobilenode = 'dulogs/2022', inrange = 'dulogs/2022')
 patterns <- c(mobilenode = 'MN_DATA', inrange = 'MN_RANGE')
+dir.create('logs/read_dulogs')
 
+#-------------------------------------------------------------------------------
+# OR
+#-------------------------------------------------------------------------------
 # with time epsilon of 0: 63 seconds
 tictoc::tic()
 read_dulogs(
@@ -12,7 +16,7 @@ read_dulogs(
   year = 2022,
   time_eps = 0,
   barn_rule = 'or',
-  log = 'logs/read_dulogs/time_eps0'
+  log = 'logs/read_dulogs/timeeps0_barnruleor'
 )
 tictoc::toc()
 
@@ -24,7 +28,7 @@ read_dulogs(
   year = 2022,
   time_eps = 1,
   barn_rule = 'or',
-  log = 'logs/read_dulogs/time_eps1'
+  log = 'logs/read_dulogs/timeeps1_barnruleor'
 )
 tictoc::toc()
 
@@ -36,7 +40,7 @@ read_dulogs(
   year = 2022,
   time_eps = 2,
   barn_rule = 'or',
-  log = 'logs/read_dulogs/time_eps2'
+  log = 'logs/read_dulogs/timeeps2_barnruleor'
 )
 tictoc::toc()
 
@@ -48,6 +52,57 @@ read_dulogs(
   year = 2022,
   time_eps = 3,
   barn_rule = 'or',
-  log = 'logs/read_dulogs/time_eps3'
+  log = 'logs/read_dulogs/timeeps3_barnruleor'
+)
+tictoc::toc()
+
+#-------------------------------------------------------------------------------
+# AND
+#-------------------------------------------------------------------------------
+# with time epsilon of 0: 63 seconds
+tictoc::tic()
+read_dulogs(
+  paths = paths,
+  patterns = patterns,
+  year = 2022,
+  time_eps = 0,
+  barn_rule = 'and',
+  log = 'logs/read_dulogs/timeeps0_barnruleand'
+)
+tictoc::toc()
+
+# with time epsilon of 1: 62 seconds
+tictoc::tic()
+read_dulogs(
+  paths = paths,
+  patterns = patterns,
+  year = 2022,
+  time_eps = 1,
+  barn_rule = 'and',
+  log = 'logs/read_dulogs/timeeps1_barnruleand'
+)
+tictoc::toc()
+
+# with time epsilon of 2: 63 seconds
+tictoc::tic()
+read_dulogs(
+  paths = paths,
+  patterns = patterns,
+  year = 2022,
+  time_eps = 2,
+  barn_rule = 'and',
+  log = 'logs/read_dulogs/timeeps2_barnruleand'
+)
+tictoc::toc()
+
+# with time epsilon of 3: 63 seconds
+tictoc::tic()
+read_dulogs(
+  paths = paths,
+  patterns = patterns,
+  year = 2022,
+  time_eps = 3,
+  barn_rule = 'and',
+  log = 'logs/read_dulogs/timeeps3_barnruleand'
 )
 tictoc::toc()
